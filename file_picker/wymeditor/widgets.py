4467ad547bad232151407eeb9f7285fb1bf0d2e6
Evan Culver <evan@evanculver.com>
2012-01-10 14:59:55 -0800

    Making jQuery URL configurable via settings.

--- file_picker/wymeditor/widgets.py
+++ file_picker/wymeditor/widgets.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from file_picker.widgets import FilePickerWidget
 
 class WYMeditorWidget(FilePickerWidget):
@@ -6,11 +7,15 @@ class WYMeditorWidget(FilePickerWidget):
         super(WYMeditorWidget, self).__init__(pickers, *args, **kwargs)
 
     class Media:
-        css = {"all": ("file_picker/css/filepicker.overlay.css",
-                       "mediaelement/mediaelementplayer.min.css",)}
-        js = ("wymeditor/jquery.wymeditor.js",
-              "wymeditor/jquery.wymeditor.filepicker.js",
-              "wymeditor/plugins/embed/jquery.wymeditor.embed.js",
-              "file_picker/js/ajaxupload.js",
-              "file_picker/js/jquery.filepicker.js",
-              "mediaelement/mediaelement-and-player.min.js",)
+        css = {'all': ('file_picker/css/filepicker.overlay.css',
+                       'mediaelement/mediaelementplayer.min.css',)}
+        js = (
+            getattr(settings, 'JQUERY_URL',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js'),
+            'wymeditor/jquery.wymeditor.js',
+            'wymeditor/jquery.wymeditor.filepicker.js',
+            'wymeditor/plugins/embed/jquery.wymeditor.embed.js',
+            'file_picker/js/ajaxupload.js',
+            'file_picker/js/jquery.filepicker.js',
+            'mediaelement/mediaelement-and-player.min.js',
+        )
