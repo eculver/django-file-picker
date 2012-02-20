462077edd5fbc192dcd9aa61fccf66591ca0c73e
Evan Culver <evan@evanculver.com>
2012-02-20 13:17:32 -0800

    Make default image list thumbnails smaller.

--- file_picker/settings.py
+++ file_picker/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
 NOT_FOUND_STRING = getattr(settings, 'FILE_PICKER_NOT_FOUND_STRING', 'Not Found')
-THUMBNAIL_SIZE = getattr(settings, 'FILE_PICKER_THUMBNAIL_SIZE', '300x300')
+THUMBNAIL_SIZE = getattr(settings, 'FILE_PICKER_THUMBNAIL_SIZE', '100x100')
 MEDIA_URL = getattr(settings, 'FILE_PICKER_MEDIA_URL', settings.MEDIA_URL)
 STATIC_URL = getattr(settings, 'FILE_PICKER_STATIC_URL', settings.STATIC_URL)
